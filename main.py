c09c0ed9115276f8862335e3091d70ac4c32862a
John <128350391+alpaOo@users.noreply.github.com>
2025-06-21 05:12:32 +0800

    Add user query and input validation

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-# Load environment variables.
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+
+def main():
+    # Load environment variables.
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    # Extract user argument from input. Must have at least 1 argument present.
+    if len(sys.argv) < 2:
+        print("Error: Must have at least 1 argument present.")
+        sys.exit(1)
+
+    _, user_content = sys.argv
+
+    # Create Google genai client.
+    client = genai.Client(api_key=api_key)
+
+    # Generate a response from Goolge genai.
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=user_content,
+    )
+
+    print(response.text)
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
-# Create Google genai client.
-client = genai.Client(api_key=api_key)
-
-# Generate a response from Goolge genai.
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-)
-
-print(response.text)
-
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+main()
