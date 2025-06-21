21720619c55e9312a72a10a101cc7f09424c45b4
John <128350391+alpaOo@users.noreply.github.com>
2025-06-22 02:59:06 +0800

    Retreive and display item information

---
+++ functions/get_files_info.py
@@ -0,0 +1,19 @@
+import os
+
+
+def get_files_info(working_directory, directory=None):
+    root_path = os.path.abspath(working_directory)
+    requested_path = os.path.abspath(os.path.join(root_path, directory))
+
+    # If requested path is outside the working directory, return error
+    if not requested_path.startswith(root_path):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if not os.path.isdir(requested_path):
+        return f'Error: "{directory}" is not a directory'
+
+    for item in os.listdir(requested_path):
+        file_size = os.path.getsize(os.path.join(requested_path, item))
+        is_dir = os.path.isdir(os.path.join(requested_path, item))
+
+        print(f"{item}: file_size={file_size}, is_dir={is_dir}")
