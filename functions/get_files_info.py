12851c25f669634b273b66f66317311802a8ce05
John <128350391+alpaOo@users.noreply.github.com>
2025-06-23 16:59:09 +0800

    Fix logic for optional directory arg

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,14 +3,18 @@ import os
 
 def get_files_info(working_directory, directory=None):
     root_path = os.path.abspath(working_directory)
-    requested_path = os.path.abspath(os.path.join(root_path, directory))
+    requested_path = directory
 
-    # If requested path is outside the working directory, return error
-    if not requested_path.startswith(root_path):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if directory:
+        requested_path = os.path.abspath(os.path.join(root_path, directory))
+        # If requested path is outside the working directory, return error
+        if not requested_path.startswith(root_path):
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not os.path.isdir(requested_path):
-        return f'Error: "{directory}" is not a directory'
+        if not os.path.isdir(requested_path):
+            return f'Error: "{directory}" is not a directory'
+    else:
+        requested_path = root_path
 
     for item in os.listdir(requested_path):
         file_size = os.path.getsize(os.path.join(requested_path, item))
